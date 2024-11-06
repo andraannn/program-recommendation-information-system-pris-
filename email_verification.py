993fd54a58cb6704a69caf7b11b9e5d25408dacb
Andrei Adlawan <andreigabrielle.adlawan@g.msuiit.edu.ph>
2024-11-06 11:10:54 +0800

    modified email design for verification and forgot password

--- email_verification.py
+++ email_verification.py
@@ -9,9 +9,9 @@ def send_verification_email(email, verification_token):
     <head>
       <style>
         .button {{
-          background-color: #b4d4fa;
+          background-color: #a41d21;
           border: none;
-          color: #000000; 
+          color: #ffffff !important; 
           text-align: center;
           text-decoration: none;
           display: inline-block;
@@ -21,22 +21,42 @@ def send_verification_email(email, verification_token):
           border-radius: 12px;
           padding: 10px 24px;
         }}
+        .card {{
+          margin: 0 auto;
+          width: 600px;
+          background: #f4f3e3;
+          border-radius: 10px;
+          box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.05);
+          box-sizing: border-box;
+        }}
+        .image-container {{
+          text-align: center;
+        }}
+        .image-container img {{
+          width: 100px;
+          border-radius: 10px 10px 0 0;
+        }}
       </style>
     </head>
     <body>
- 
-      <p>You have successfully created an account.</p>
-      <p>Please click the button below to verify your email:</p>
-      <a href="{verification_link}" class="button">Verify Email</a>
-      <br><br>
-      <p>If the button above does not work, you can also click the link below:</p>
-      <p><a href="{verification_link}">{verification_link}</a></p>
-      
+      <div class="card">
+        <div class="image-container" style="padding-top: 20px;">
+          <img src="https://www.msuiit.edu.ph/assets/img/seal-02.png" style="max-width: 100%; border-radius: 10px 10px 0 0;" alt="Image"> 
+        </div>
+        <div style="padding: 20px;">
+          <p style="text-align: center; font-family: 'Poppins', sans-serif; font-weight: 500; font-size: 18px; line-height: 1.5;">You have successfully created an account.</p>
+          <p style="text-align: center; font-family: 'Poppins', sans-serif; font-weight: 500; font-size: 18px; line-height: 1.5;">Please click the button below to verify your email:</p>
+          <a href="{verification_link}" class="button" style="display: block; margin: 0 auto;">Verify Email</a>
+          <br><br>
+          <p>If the button above does not work, you can also click the link below:</p>
+          <p><a href="{verification_link}">{verification_link}</a></p>
+        </div>
+      </div>
     </body>
     </html>
     """
 
-    msg = Message("CRIS Registration Email Verification", recipients=[email])
+    msg = Message("PRIS Registration Email Verification", recipients=[email])
     msg.body = message
     msg.html = message  # Use the HTML version for better formatting
     mail.send(msg)
--- forgot_password.py
+++ forgot_password.py
@@ -12,9 +12,9 @@ def send_password_reset_email(email, reset_token):
     <head>
       <style>
         .button {{
-          background-color: #b4d4fa;
+          background-color: #a41d21;
           border: none;
-          color: #000000; 
+          color: #ffffff !important; 
           text-align: center;
           text-decoration: none;
           display: inline-block;
@@ -24,22 +24,42 @@ def send_password_reset_email(email, reset_token):
           border-radius: 12px;
           padding: 10px 24px;
         }}
+        .card {{
+          margin: 0 auto;
+          width: 600px;
+          background: #f4f3e3;
+          border-radius: 10px;
+          box-shadow: 10px 10px 10px 10px rgba(0, 0, 0, 0.05);
+          box-sizing: border-box;
+        }}
+         .image-container {{
+          text-align: center;
+        }}
+        .image-container img {{
+          width: 100px;
+          border-radius: 10px 10px 0 0;
+        }}
       </style>
     </head>
     <body>
-      
-      <p>You have requested to reset your account password.</p>
-      <p>Please click the button below to reset your account password:</p>
-      <a href="{reset_link}" class="button">Reset Password</a>
-      <br><br>
-      <p>If the button above does not work, you can also click the link below:</p>
-      <p><a href="{reset_link}">{reset_link}</a></p>
-      
+      <div class="card">
+        <div class="image-container" style="padding-top: 20px;">
+          <img src="https://www.msuiit.edu.ph/assets/img/seal-02.png" style="max-width: 100%; border-radius: 10px 10px 0 0;" alt="Image"> 
+        </div>
+        <div style="padding: 20px;">
+          <p style="text-align: center; font-family: 'Poppins', sans-serif; font-weight: 500; font-size: 18px; line-height: 1.5;">You have requested to reset your account password.</p>
+          <p style="text-align: center; font-family: 'Poppins', sans-serif; font-weight: 500; font-size: 18px; line-height: 1.5;">Please click the button below to reset your account password:</p>
+          <a href="{reset_link}" class="button" style="display: block; margin: 0 auto;">Reset Password</a>
+          <br><br>
+          <p>If the button above does not work, you can also click the link below:</p>
+          <p><a href="{reset_link}">{reset_link}</a></p>
+        </div>
+      </div>
     </body>
     </html>
     """
 
-    msg = Message("CRIS Reset Password Request", recipients=[email])
+    msg = Message("PRIS Reset Password Request", recipients=[email])
     msg.body = message
     msg.html = message  # Use the HTML version for better formatting
     mail.send(msg)
